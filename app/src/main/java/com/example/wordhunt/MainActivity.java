9baa4072412b1e8a8c5badbb6adff61ce2374f03
SamiaShashmi <shoshmiislam149@gmail.com>
2021-02-03 19:39:41 +0600

    level change done

--- app/src/main/java/com/example/wordhunt/MainActivity.java
+++ app/src/main/java/com/example/wordhunt/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,10 +18,16 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView dataList;
     List<String> letters;
     Adapter adapter;
+    public static int totalLevelCount = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        gridGenerator();
+
+
+    }
+    public void gridGenerator(){
         dataList = findViewById(R.id.dataList);
 
         letters = new ArrayList<>();
@@ -35,7 +44,19 @@ public class MainActivity extends AppCompatActivity {
         GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 8, GridLayoutManager.VERTICAL, false);
         dataList.setLayoutManager(gridLayoutManager);
         dataList.setAdapter(adapter);
-
     }
 
+
+    public void levelUp(View view) {
+        totalLevelCount++;
+        TextView textLevel = (TextView) findViewById(R.id.levelCount);
+        String totalString = textLevel.getText().toString();
+        String levelString = totalString.substring(0, totalString.length() - 4);
+        int currentlevel = Integer.parseInt(levelString) + 1;
+        String newLevel = Integer.toString(currentlevel) + "/100";
+        textLevel.setText(newLevel);
+        TextView prevWord = (TextView) findViewById(R.id.madeWord);
+        prevWord.setText("");
+        gridGenerator();
+    }
 }
