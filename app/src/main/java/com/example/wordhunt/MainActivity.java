07c0fce3c7b370cbb348f5bc52fb2a5d7de1b59d
SamiaShashmi <shoshmiislam149@gmail.com>
2021-02-04 21:32:28 +0600

    valid word reservation and highlighting done

--- app/src/main/java/com/example/wordhunt/MainActivity.java
+++ app/src/main/java/com/example/wordhunt/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.animation.ArgbEvaluator;
 import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
@@ -13,6 +15,7 @@ import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.view.animation.LinearInterpolator;
+import android.view.animation.ScaleAnimation;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -38,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        gridGenerator();
+        gridGenerator(totalLevelCount);
 
         countdownText = findViewById(R.id.timer);
         startTimer();
@@ -46,16 +49,87 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-    public void gridGenerator(){
+    public void gridGenerator(int totalLevelCount){
         dataList = findViewById(R.id.dataList);
 
         letters = new ArrayList<>();
         Random r = new Random();
 
         String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        String forDivByFive = "home";
+        String forDivByFour = "plan";
+        String forDivByThree = "goat";
+        String forRest = "seat";
         for (int i = 0; i < 64; i++) {
-            char rand = alphabet.charAt(r.nextInt(alphabet.length()));
-            letters.add("" + rand);
+            if(i == 10 || i == 20 || i == 40 || i == 50)
+            {
+                if(totalLevelCount % 5 == 0 && i == 10){
+                    letters.add("" + "h");
+                }
+                else if(totalLevelCount % 5 == 0 && i == 20){
+                    letters.add("" + "o");
+                }
+                else if(totalLevelCount % 5 == 0 && i == 40){
+                    letters.add("" + "m");
+                }
+                else if(totalLevelCount % 5 == 0 && i == 50){
+                    letters.add("" + "e");
+                }
+                else if(totalLevelCount % 4 == 0 && i == 10){
+                    letters.add("" + "p");
+                }
+                else if(totalLevelCount % 4 == 0 && i == 20){
+                    letters.add("" + "l");
+                }
+                else if(totalLevelCount % 4 == 0 && i == 40){
+                    letters.add("" + "a");
+                }
+                else if(totalLevelCount % 4 == 0 && i == 50){
+                    letters.add("" + "n");
+                }
+                else if(totalLevelCount % 3 == 0 && i == 10){
+                    letters.add("" + "g");
+                }
+                else if(totalLevelCount % 3 == 0 && i == 20){
+                    letters.add("" + "o");
+                }
+                else if(totalLevelCount % 3 == 0 && i == 40){
+                    letters.add("" + "a");
+                }
+                else if(totalLevelCount % 3 == 0 && i == 50){
+                    letters.add("" + "t");
+                }
+                else if(totalLevelCount % 2 == 0 && i == 10){
+                    letters.add("" + "b");
+                }
+                else if(totalLevelCount % 2 == 0 && i == 20){
+                    letters.add("" + "o");
+                }
+                else if(totalLevelCount % 2 == 0 && i == 40){
+                    letters.add("" + "a");
+                }
+                else if(totalLevelCount % 2 == 0 && i == 50){
+                    letters.add("" + "t");
+                }
+                else if(i == 10){
+                    letters.add("" + "s");
+                }
+                else if(i == 20){
+                    letters.add("" + "e");
+                }
+                else if(i == 40){
+                    letters.add("" + "a");
+                }
+                else if(i == 50){
+                    letters.add("" + "t");
+                }
+            }
+            else
+            {
+                char rand = alphabet.charAt(r.nextInt(alphabet.length()));
+                letters.add("" + rand);
+            }
+
         }
 
 
@@ -76,7 +150,7 @@ public class MainActivity extends AppCompatActivity {
         textLevel.setText(newLevel);
         TextView prevWord = (TextView) findViewById(R.id.madeWord);
         prevWord.setText("");
-        gridGenerator();
+        gridGenerator(totalLevelCount);
         resetTimer();
     }
 
@@ -109,8 +183,10 @@ public class MainActivity extends AppCompatActivity {
         if(seconds <= 5)
         {
             clockBlink();
+            highlight();
         }
     }
+
     private void levelChange()
     {
         totalLevelCount++;
@@ -122,7 +198,7 @@ public class MainActivity extends AppCompatActivity {
         textLevel.setText(newLevel);
         TextView prevWord = (TextView) findViewById(R.id.madeWord);
         prevWord.setText("");
-        gridGenerator();
+        gridGenerator(totalLevelCount);
         resetTimer();
     }
 
@@ -177,5 +253,24 @@ public class MainActivity extends AppCompatActivity {
         animation.setRepeatMode(Animation.REVERSE); //animation will start from end point once ended.
         clock.startAnimation(animation); //to start animation
 
+    }
+
+    private void highlight()
+    {
+        RecyclerView recyclerView = findViewById(R.id.dataList);
+        View highlight1 = recyclerView.findViewHolderForAdapterPosition(10).itemView;
+        View highlight2 = recyclerView.findViewHolderForAdapterPosition(20).itemView;
+        View highlight3 = recyclerView.findViewHolderForAdapterPosition(40).itemView;
+        View highlight4 = recyclerView.findViewHolderForAdapterPosition(50).itemView;
+        Animation animation = new ScaleAnimation(
+                1f, 1.2f, // Start and end values for the X axis scaling
+                1f, 1.2f, // Start and end values for the Y axis scaling
+                Animation.RELATIVE_TO_SELF, 1f, // Pivot point of X scaling
+                Animation.RELATIVE_TO_SELF, 1f); // Pivot point of Y scaling
+        animation.setDuration(1000);
+        highlight1.startAnimation(animation);
+        highlight2.startAnimation(animation);
+        highlight3.startAnimation(animation);
+        highlight4.startAnimation(animation);
     }
 }
