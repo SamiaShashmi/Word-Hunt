48a875e0a49bd614253d2798c9123814737b5b70
SamiaShashmi <shoshmiislam149@gmail.com>
2021-02-08 03:36:44 +0600

    pop up resume done

--- app/src/main/java/com/example/wordhunt/MainActivity.java
+++ app/src/main/java/com/example/wordhunt/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.wordhunt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.app.Dialog;
 import android.content.Intent;
 import android.media.MediaPlayer;
@@ -9,17 +10,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioGroup;
+
+import static com.example.wordhunt.mainGrid.isOver;
 
 public class MainActivity extends AppCompatActivity {
 
     Button NewGame;
     Button Resume;
     Button HighScore;
-    Button Exit;
     Button Help;
     Dialog playerNameWindow;
     static Boolean sound_check = false;
-    MediaPlayer song;
+    public MediaPlayer song;
+    RadioGroup radioGroup;
+    public int radioChecked = 1;
+    ImageView muteimg;
+    ImageView unmuteimg;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,8 +39,13 @@ public class MainActivity extends AppCompatActivity {
         Exit=findViewById(R.id.exit);
         Help=findViewById(R.id.help);*/
 
+
         playerNameWindow = new Dialog(MainActivity.this);
         playerNameWindow.setContentView(R.layout.player_info_pop_up);
+
+        muteimg = findViewById(R.id.mute);
+        unmuteimg = findViewById(R.id.unmute);
+        unmuteimg.setVisibility(View.INVISIBLE);
 
         if(sound_check == false)
         {
@@ -47,39 +61,15 @@ public class MainActivity extends AppCompatActivity {
                 playerNameWindow.show();
             }
         });
-        /*HighScore.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
-            }
-        });*/
-        /*Resume.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent2 = new Intent(MainActivity.this, FromResume.class);
-                startActivity(intent2);
-            }
-        });
-
+        ImageView Exit = findViewById(R.id.exitbtn);
         Exit.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent GoAway = new Intent(getApplicationContext(),MainActivity.class);
-                GoAway.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                GoAway.putExtra("EXIT",true);
-                startActivity(GoAway);
-
-                finish();
+            public void onClick(View v) {
+             finish();
                 System.exit(0);
             }
-        });*/
-        /*Help.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent4 = new Intent(MainActivity.this, Help1.class);
-                startActivity(intent4);
-            }
-        });*/
+        });
 
     }
     public void openGame(View view)
@@ -89,8 +79,29 @@ public class MainActivity extends AppCompatActivity {
         playerNameWindow.dismiss();
         Intent intent = new Intent(MainActivity.this, mainGrid.class);
         intent.putExtra("playerName", playerNameFromPopUp);
+        intent.putExtra("level", radioChecked);
+        song.pause();
+        sound_check = false;
         startActivity(intent);
     }
+    public void checkButton(View view)
+    {
+        radioGroup = playerNameWindow.findViewById(R.id.radio);
+        int radioId = 0;
+        radioId = radioGroup.getCheckedRadioButtonId();
+        switch (radioId) {
+            case R.id.easy:
+                radioChecked = 1;
+                break;
+            case R.id.medium:
+                radioChecked = 2;
+                break;
+            case R.id.hard:
+                radioChecked = 3;
+                break;
+        }
+    }
+
     public void openleaderBoard(View view)
     {
         Intent intent3 = new Intent(MainActivity.this, HighScore.class);
@@ -102,6 +113,40 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent3);
     }
 
+
+    @Override
+    public void onBackPressed() {
+        finish();
+        System.exit(0);
+
+
+    }
+    public void mute(View view)
+    {
+        if(sound_check == true)
+        {
+            song.pause();
+            sound_check = false;
+            muteimg.setVisibility(View.INVISIBLE);
+            unmuteimg.setVisibility(View.VISIBLE);
+        }
+
+    }
+    public void unmute(View view)
+    {
+        if(sound_check == false)
+        {
+            song.start();
+            sound_check = true;
+            muteimg.setVisibility(View.VISIBLE);
+            unmuteimg.setVisibility(View.INVISIBLE);
+        }
+    }
+    public void resumeMainMenu(View view) {
+        isOver = false;
+        Intent intent = new Intent(MainActivity.this, mainGrid.class);
+        startActivity(intent);
+    }
 }
 
 
